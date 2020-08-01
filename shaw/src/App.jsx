5be0c248c44b11826796d4cc3f2202326cd02e5a
Kalvin Mateo <kalvinmateo16@gmail.com>
2020-08-01 00:17:18 -0700

    Setup is almost complete

--- shaw/src/App.jsx
+++ shaw/src/App.jsx
@@ -3,11 +3,14 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav } from 'react-bootstrap';
+import { ReactComponent as Logo } from "./ui/images/iconmonstr-medical-6.svg";
 
-import './ui/CSS/App.css';
+// import './ui/CSS/App.css';
+import './ui/SCSS/App.scss';
 import Home from "./ui/Home";
 import About from "./ui/About";
 import Calendar from "./ui/Calendar";
+import Feature from "./ui/Features";
 import NotFound from "./ui/NotFound";
 
 class App extends Component {
@@ -16,11 +19,15 @@ class App extends Component {
             <div className="app">
                 <BrowserRouter>
                     <div className="nav-bar-app">
-                        <Navbar bg="dark" variant="dark">
-                            <Navbar.Brand href="/">SHAW</Navbar.Brand>
+                        <Navbar id="nav-bar-app" bg="dark" variant="dark">
+                            <Navbar.Brand id="brand" href="/" >
+                                <Logo alt="" width="25px" height="25px" className="logo" />
+                                SHAW
+                            </Navbar.Brand>
                             <Nav className="mr-auto">
                                 <Nav.Link href="/about">About</Nav.Link>
                                 <Nav.Link href="/calendar">Calendar</Nav.Link>
+                                <Nav.Link href="/feature">Features</Nav.Link>
                             </Nav>
                         </Navbar>
                     </div>
@@ -28,6 +35,7 @@ class App extends Component {
                         <Route exact path="/" component={Home} />
                         <Route path="/about" component={About} />
                         <Route path="/calendar" component={Calendar} />
+                        <Route path="/feature" component={Feature} />
                         <Route component={NotFound} />
                     </Switch>
                 </BrowserRouter>
---
+++ shaw/src/ui/Features.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Feature extends Component {
+    render(){
+        return(
+            <div className="feature">
+                I am Feature
+            </div>
+        );
+    }
+}
+
+export default Feature;
